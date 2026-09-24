c46b7703e453f9957919c72132a7734db94eb280
Arafat Hossen Sabbir <ahsabbir476@gmail.com>
2026-09-24 15:52:01 +0600

    deployed url added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://your-client-deployed-url.com"],
     credentials: true,
   }),
 );
